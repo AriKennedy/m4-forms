5873e080f6e2ccb9dd340974319e15865bdf89cf
Arianna Kennedy <akennedy25@basecampcodingacademy.org>
2024-11-15 14:45:03 -0600

    Added TestCases for codingbat_over_http

--- codingbat_over_http/app/tests.py
+++ codingbat_over_http/app/tests.py
@@ -1,3 +1,52 @@
-from django.test import TestCase
+# warmups/tests.py
+from django.test import SimpleTestCase
 
-# Create your tests here.
+class TestFrontTimes(SimpleTestCase):
+    def test_front_times_boo(self):
+        response = self.client.get('/front-times/?csrfmiddlewaretoken=eCTJUQtmkn2JHg1l03dZIB86ByLFxVMCa992sLu9cG4bxzWLXcdapa40GtX1opDT&text=boo&n=2')
+        self.assertContains(response, 'Result: booboo')
+
+    def test_front_times_Abc(self):
+        response = self.client.get('/front-times/?csrfmiddlewaretoken=eCTJUQtmkn2JHg1l03dZIB86ByLFxVMCa992sLu9cG4bxzWLXcdapa40GtX1opDT&text=Abc&n=3')
+        self.assertContains(response, 'Result: AbcAbcAbc')
+
+    def test_front_times_Chocolate(self):
+        response = self.client.get('/front-times/?csrfmiddlewaretoken=eCTJUQtmkn2JHg1l03dZIB86ByLFxVMCa992sLu9cG4bxzWLXcdapa40GtX1opDT&text=Chocolate&n=3')
+        self.assertContains(response, 'Result: ChoChoCho')
+
+class TestNoTeenSums(SimpleTestCase):
+    def test_no_teen_sums_1_2_3(self):
+        response = self.client.get('/no-teen-sums/?csrfmiddlewaretoken=dn5hAnXW8CaKZPlNqfiIF5UPAJOiFkOJ9UlA8iYJ0VccP8gdnoiTmEQJFE0EwOF0&a=1&b=2&c=3')
+        self.assertContains(response, 'Result: 6')
+
+    def test_no_teen_sums_2_13_1(self):
+        response = self.client.get('/no-teen-sums/?csrfmiddlewaretoken=dn5hAnXW8CaKZPlNqfiIF5UPAJOiFkOJ9UlA8iYJ0VccP8gdnoiTmEQJFE0EwOF0&a=2&b=13&c=1')
+        self.assertContains(response, 'Result: 3')
+
+    def test_no_teen_sums_2_1_14(self):
+        response = self.client.get('/no-teen-sums/?csrfmiddlewaretoken=dn5hAnXW8CaKZPlNqfiIF5UPAJOiFkOJ9UlA8iYJ0VccP8gdnoiTmEQJFE0EwOF0&a=2&b=1&c=14')
+        self.assertContains(response, 'Result: 3')
+
+class TestXyzThere(SimpleTestCase):
+    def test_xyz_there_abcxyz(self):
+        response = self.client.get('/xyz-there/?csrfmiddlewaretoken=NQaHjiyq4fAeAzFLOqErjjzKU6byWmQjJnq0RdzdWyCGqSAbLzEC0SvEZ1nUNQHA&text=abcxyz')
+        self.assertContains(response, 'Result: True')
+
+    def test_xyz_there_abc_xyz(self):
+        response = self.client.get('/xyz-there/?csrfmiddlewaretoken=NQaHjiyq4fAeAzFLOqErjjzKU6byWmQjJnq0RdzdWyCGqSAbLzEC0SvEZ1nUNQHA&text=abc.xyz')
+        self.assertContains(response, 'Result: False')
+
+    def test_xyz_there_xyz(self):
+        response = self.client.get('/xyz-there/?csrfmiddlewaretoken=NQaHjiyq4fAeAzFLOqErjjzKU6byWmQjJnq0RdzdWyCGqSAbLzEC0SvEZ1nUNQHA&text=xyz.abc')
+        self.assertContains(response, 'Result: True')
+
+class TestCenteredAverage(SimpleTestCase):
+    def test_centered_average_1_2_3_4_100(self):
+        response = self.client.get('/centered-average/?csrfmiddlewaretoken=lBM1JMHOTzrqudLUtIqgFBVuS4R0V3uIh82khHIBLStSkwGkqRqrmaRoXZ3mMxlZ&numbers=1%2C+2%2C+3%2C+4%2C+100')
+        self.assertContains(response, 'Result: 3')
+    def test_centered_average_1_1_5_5_10_8_7(self):
+        response = self.client.get('/centered-average/?csrfmiddlewaretoken=lBM1JMHOTzrqudLUtIqgFBVuS4R0V3uIh82khHIBLStSkwGkqRqrmaRoXZ3mMxlZ&numbers=1%2C+1%2C+5%2C+5%2C+10%2C+8%2C+7')
+        self.assertContains(response, 'Result: 5')
+    def test_centered_average_10_4_2_4_2_0(self):
+        response = self.client.get('/centered-average/?csrfmiddlewaretoken=lBM1JMHOTzrqudLUtIqgFBVuS4R0V3uIh82khHIBLStSkwGkqRqrmaRoXZ3mMxlZ&numbers=-10%2C+-4%2C+-2%2C+-4%2C+-2%2C+0')
+        self.assertContains(response, 'Result: -3')
--- codingbat_over_http/app/views.py
+++ codingbat_over_http/app/views.py
@@ -6,57 +6,58 @@ from .forms import FrontTimesForm, NoTeenSumsForm, XyzThereForm, CenteredAverage
 def home_view(request):
     return render(request, 'home.html')
 
-# Front Times View
+# Front Times
 def front_times_view(request):
-    if request.method == 'POST':
-        form = FrontTimesForm(request.POST)
+    if request.method == 'GET':
+        form = FrontTimesForm(request.GET)
         if form.is_valid():
             text = form.cleaned_data['text']
             n = form.cleaned_data['n']
-            front = text[:3]  # First 3 characters or less if text is shorter
-            result = front * n  # Repeat the front n times
+            front = text[:3]
+            result = front * n
             return HttpResponse(f"Result: {result}")
-    else:
-        form = FrontTimesForm()
-    return render(request, 'front_times.html', {'form': form})
+        else:
+            form = FrontTimesForm()
+        return render(request, 'front_times.html', {'form': form})
 
-# No Teen Sums View
+# No Teen Sums
 def no_teen_sums_view(request):
-    if request.method == 'POST':
-        form = NoTeenSumsForm(request.POST)
+    if request.method == 'GET':
+        form = NoTeenSumsForm(request.GET)
         if form.is_valid():
-            a = form.cleaned_data['a']
-            b = form.cleaned_data['b']
-            c = form.cleaned_data['c']
+            a, b, c = form.cleaned_data['a'], form.cleaned_data['b'], form.cleaned_data['c']
             def fix_teen(n):
                 return 0 if n in [13, 14, 17, 18, 19] else n
             result = fix_teen(a) + fix_teen(b) + fix_teen(c)
             return HttpResponse(f"Result: {result}")
-    else:
-        form = NoTeenSumsForm()
-    return render(request, 'no_teen_sums.html', {'form': form})
+        else:
+            form = NoTeenSumsForm()
+        return render(request, 'no_teen_sums.html', {'form': form})
 
-# XYZ There View
+# XYZ There
 def xyz_there_view(request):
-    if request.method == 'POST':
-        form = XyzThereForm(request.POST)
+    if request.method == 'GET':
+        form = XyzThereForm(request.GET)
         if form.is_valid():
             text = form.cleaned_data['text']
             result = 'xyz' in text.replace('.xyz', '')
             return HttpResponse(f"Result: {result}")
-    else:
-        form = XyzThereForm()
-    return render(request, 'xyz_there.html', {'form': form})
+        else:
+            form = XyzThereForm()
+        return render(request, 'xyz_there.html', {'form': form})
 
-# Centered Average View
+# Centered Average
 def centered_average_view(request):
-    if request.method == 'POST':
-        form = CenteredAverageForm(request.POST)
+    if request.method == 'GET':
+        form = CenteredAverageForm(request.GET)
         if form.is_valid():
             numbers = list(map(int, form.cleaned_data['numbers'].split(',')))
-            numbers.sort()
-            result = sum(numbers[1:-1]) // (len(numbers) - 2)  # Average excluding largest & smallest
+            def centered_average(nums):
+                small = min(nums)
+                big = max(nums)
+                return (sum(nums) - small - big) // (len(nums) - 2)
+            result = centered_average(numbers)
             return HttpResponse(f"Result: {result}")
-    else:
-        form = CenteredAverageForm()
-    return render(request, 'centered_average.html', {'form': form})
+        else:
+            form = CenteredAverageForm()
+        return render(request, 'centered_average.html', {'form': form})
